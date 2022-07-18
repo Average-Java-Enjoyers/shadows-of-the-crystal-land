ae6796b79bd6d133ea100eca6e7c42c9147efa63
erykm <01159054@pw.edu.pl>
2022-07-18 22:22:16 +0200

    java 17 version, custom screen adapter

--- desktop/src/com/averagejavaenjoyers/game/DesktopLauncher.java
+++ desktop/src/com/averagejavaenjoyers/game/DesktopLauncher.java
@@ -2,7 +2,6 @@ package com.averagejavaenjoyers.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.averagejavaenjoyers.game.ShadowsOfTheCrystalLandGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -10,6 +9,6 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Shadows of the Crystal Land");
-		new Lwjgl3Application(new ShadowsOfTheCrystalLandGame(), config);
+		new Lwjgl3Application(ShadowsOfTheCrystalLandGame.instance(), config);
 	}
 }
