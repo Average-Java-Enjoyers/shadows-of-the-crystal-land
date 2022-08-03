eaa9fb59fbcf86ce3567a101c53035a6d2fc6087
mpr <235953@edu.p.lodz.pl>
2022-08-03 23:38:32 +0200

    dziala przelaczanie pomiedzy screenami tj klikajac przycisk exit wyskakuje popup a klikajac single przenosi nas do LevelScreena naprawa renderowania mapy (nalezalo jeszcze zdefiniowac obiekty cegieł, rur itp na mapie) MAPA JEST TYLKO PRZYKLADEM

--- core/src/com/averagejavaenjoyers/game/screen/LevelScreen.java
+++ core/src/com/averagejavaenjoyers/game/screen/LevelScreen.java
@@ -5,26 +5,106 @@ import com.averagejavaenjoyers.game.ShadowsOfTheCrystalLandGame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.averagejavaenjoyers.game.ShadowsOfTheCrystalLandGame;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import static com.averagejavaenjoyers.game.ShadowsOfTheCrystalLandGame.SCREEN_HEIGHT;
+import static com.averagejavaenjoyers.game.ShadowsOfTheCrystalLandGame.SCREEN_WIDTH;
+
 public class LevelScreen extends CustomScreen {
-    private TmxMapLoader tmxMapLoader = new TmxMapLoader();
-    private TiledMap map;
-    private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
-    private GameLogic levelLogic;
-    private Viewport gamePort;
     private OrthographicCamera gameCam;
+    private Viewport gamePort;
+
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+    private World world;
+    private Box2DDebugRenderer b2dr; //reprezentacja obiektow w swiecie
+
 
 
     public LevelScreen(){
-        tmxMapLoader = new TmxMapLoader();
-        map = tmxMapLoader.load("map.tmx");
-        orthogonalTiledMapRenderer = new OrthogonalTiledMapRenderer(map);    //pokazanie mapy
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(SCREEN_WIDTH, SCREEN_HEIGHT, gameCam);     //do skalowania ekranu
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("map.tmx");    //ladowanie mapy zrobionej w tiled
+        renderer = new OrthogonalTiledMapRenderer(map);    //pokazanie mapy
         gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);   //ustawienie pozycji kamery na srodek okna
+
+        //w konstruktorze: grawitacja i uspienie
+        world = new World(new Vector2(0, -10), true);
+        b2dr = new Box2DDebugRenderer();
+
+        //zmienne lokalne do dodanie atrybutow dla kolejnych obiektow (pipes, bricks itp)
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
+        //przejscie petla po warstwach 0-n dol-gora
+        //tworzenie rur
+        for(MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;    //obiekty nieruchome
+            bdef.position.set((rect.getX() + rect.getWidth() / 2), (rect.getY() + rect.getHeight() / 2));
+            body = world.createBody(bdef);  //tworzenie ciala o konkretnych atrybutach
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        //tworzenie cegiel
+        for(MapObject object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;    //obiekty nieruchome
+            bdef.position.set((rect.getX() + rect.getWidth() / 2), (rect.getY() + rect.getHeight() / 2));
+            body = world.createBody(bdef);  //tworzenie ciala o konkretnych atrybutach
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        //tworzenie monet
+        for(MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;    //obiekty nieruchome
+            bdef.position.set((rect.getX() + rect.getWidth() / 2), (rect.getY() + rect.getHeight() / 2));
+            body = world.createBody(bdef);  //tworzenie ciala o konkretnych atrybutach
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        //tworzenie ziemi
+        for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;    //obiekty nieruchome
+            bdef.position.set((rect.getX() + rect.getWidth() / 2), (rect.getY() + rect.getHeight() / 2));
+            body = world.createBody(bdef);  //tworzenie ciala o konkretnych atrybutach
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
     }
 
 
@@ -34,11 +114,20 @@ public class LevelScreen extends CustomScreen {
     }
     @Override
     public void render(float delta) {
-        orthogonalTiledMapRenderer.render();
+        ScreenUtils.clear(0, 1, 0, 1);
+
+        world.step(1/60f, 6, 2);    //odswierzanie swiata
+        //gameCam.position.y = player.b2body.getPosition().y;
+
+        gameCam.update();
+        renderer.setView(gameCam);
+
+        renderer.render();  //renderowanie mapy
+        b2dr.render(world, gameCam.combined);   //renderowanie obiektow na swiecie
     }
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height);
     }
 
     @Override
