daca06aa86820a7cc1df9c10627c740f9026d5d2
mpr <235953@edu.p.lodz.pl>
2022-08-06 00:44:24 +0200

    testowanie poruszania jednostki mala zmiana mapy

--- core/src/com/averagejavaenjoyers/game/Character.java
+++ core/src/com/averagejavaenjoyers/game/Character.java
@@ -1,9 +1,46 @@
 package com.averagejavaenjoyers.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import org.w3c.dom.Text;
 
-public class Character {
+public class Character extends Sprite {
     Animation standAnimation;
+    public World world;
+    public Body b2body;
+    public TextureRegion texture;
 
+    public Character(World world){
+        this.world = world;
+        defineCharacter();
+        texture =  new TextureRegion(new Texture(Gdx.files.internal("Graphics/Cedania/CedaniaLightWarrior50x75.png")));
+    }
+
+    public void update(float dt){
+        setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
+    }
+
+    public void defineCharacter(){
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(50,50);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        b2body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        /*Image img = new Image(new Texture(Gdx.files.internal("Graphics/Cedania/CedaniaLightWarrior50x75.png")));
+        img.setWidth(50);
+        img.setHeight(75);*/
+        CircleShape shape = new CircleShape();
+        shape.setRadius(25);
+        fdef.shape = shape;
+        //shape
+        b2body.createFixture(fdef);
+    }
     
 }
--- core/src/com/averagejavaenjoyers/game/screen/LevelScreen.java
+++ core/src/com/averagejavaenjoyers/game/screen/LevelScreen.java
@@ -1,8 +1,10 @@
 package com.averagejavaenjoyers.game.screen;
 
+import com.averagejavaenjoyers.game.Character;
 import com.averagejavaenjoyers.game.GameLogic;
 import com.averagejavaenjoyers.game.ShadowsOfTheCrystalLandGame;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.MapObject;
@@ -32,6 +34,8 @@ public class LevelScreen extends CustomScreen {
     private World world;
     private Box2DDebugRenderer b2dr; //reprezentacja obiektow w swiecie
 
+    private Character player;
+
 
 
     public LevelScreen(){
@@ -44,18 +48,19 @@ public class LevelScreen extends CustomScreen {
         gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);   //ustawienie pozycji kamery na srodek okna
 
         //w konstruktorze: grawitacja i uspienie
-        world = new World(new Vector2(0, -10), true);
+        world = new World(new Vector2(0, 0), true);
         b2dr = new Box2DDebugRenderer();
 
-        //zmienne lokalne do dodanie atrybutow dla kolejnych obiektow (pipes, bricks itp)
+        player = new Character(this.world);
+
+        //zmienne lokalne do dodanie atrybutow dla kolejnych warstw obiektow
         BodyDef bdef = new BodyDef();
         PolygonShape shape = new PolygonShape();
         FixtureDef fdef = new FixtureDef();
         Body body;
 
-        //przejscie petla po warstwach 0-n dol-gora
-        //tworzenie rur
-        for(int i = 1; i <= 6 ; i++) {
+        //wyciaganie warstw obiektów z Tiled kolejność: down=0, up=n
+        for(int i = 1; i <= 5 ; i++) {
             for (MapObject object : map.getLayers().get(i).getObjects().getByType(RectangleMapObject.class)) {
                 Rectangle rect = ((RectangleMapObject) object).getRectangle();
 
@@ -70,6 +75,32 @@ public class LevelScreen extends CustomScreen {
         }
     }
 
+    public void handleInput(float dt){
+        if(Gdx.input.isKeyJustPressed(Input.Keys.UP)){
+            // w konstruktorze 1. kierunek przemieszczenie, 2. przemieszczenie ekranu, 3. obudzenie obiektu
+            player.b2body.applyLinearImpulse(new Vector2(0, 40f), player.b2body.getWorldCenter(), true);
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)){
+            // w konstruktorze 1. kierunek przemieszczenie, 2. przemieszczenie ekranu, 3. obudzenie obiektu
+            player.b2body.applyLinearImpulse(new Vector2(0, -40f), player.b2body.getWorldCenter(), true);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            player.b2body.applyLinearImpulse(new Vector2(4f,0), player.b2body.getWorldCenter(), true);
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+            player.b2body.applyLinearImpulse(new Vector2(-4f,0), player.b2body.getWorldCenter(), true);
+    }
+
+    public void update(float dt){
+        handleInput(dt);
+
+        world.step(1/60f, 6, 2);    //odswierzanie swiata
+        player.update(dt);
+        //gameCam.position.x = player.b2body.getPosition().x;
+        //gameCam.position.y = player.b2body.getPosition().y;
+
+        gameCam.update();
+        renderer.setView(gameCam);
+    }
 
     @Override
     public void show() {
@@ -78,6 +109,7 @@ public class LevelScreen extends CustomScreen {
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0, 1, 0, 1);
+        update(delta);
 
         world.step(1/60f, 6, 2);    //odswierzanie swiata
         //gameCam.position.y = player.b2body.getPosition().y;
@@ -87,6 +119,12 @@ public class LevelScreen extends CustomScreen {
 
         renderer.render();  //renderowanie mapy
         b2dr.render(world, gameCam.combined);   //renderowanie obiektow na swiecie
+
+        game.batch.setProjectionMatrix(gameCam.combined);
+        game.batch.begin();
+        //TODO: DO POPRAWKI
+            game.batch.draw(player.texture, player.b2body.getPosition().x - player.getWidth() / 2, player.b2body.getPosition().y - player.getHeight() / 2);
+        game.batch.end();
     }
     @Override
     public void resize(int width, int height) {
